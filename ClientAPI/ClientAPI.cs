f527687c7ded9b4a622103e6c06fab384f309698
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2025-04-05 17:01:02 +0200

    ClientAPI: Prepare basic API classes

---
+++ ClientAPI/ClientAPI.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace ClientAPI
+{
+    public static class Utilities
+    {
+        public static ArraySegment<byte> StringToBytes(string message)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            return new ArraySegment<byte>(buffer);
+        }
+    }
+
+    internal class ClientAPI
+    {
+    }
+}
