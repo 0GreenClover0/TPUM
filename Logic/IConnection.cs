9ff1e0668d5af001862e7589b928258a684ab2a7
MikeMG-PL <36279247+MikeMG-PL@users.noreply.github.com>
2025-04-05 17:01:51 +0200

    Logic: Add IConnection interface

---
+++ Logic/IConnection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public interface IConnection
+    {
+        public event Action? OnConnectionStateChanged;
+        public event Action<string>? OnMessage;
+        public event Action? OnError;
+
+        public Task Connect(Uri peerUri);
+        public Task Disconnect();
+
+        public bool IsConnected();
+
+        internal class Connection : Logic.IConnection
+        {
+            public event Action? OnConnectionStateChanged;
+            public event Action<string>? OnMessage;
+            public event Action? OnError;
+
+            private readonly Data.IConnection dataConnection;
+
+            public Connection(Data.IConnection connection)
+            {
+                dataConnection = connection;
+
+                connection.OnConnectionStateChanged += () => OnConnectionStateChanged?.Invoke();
+                connection.OnMessage += (message) => OnMessage?.Invoke(message);
+                connection.OnError += () => OnError?.Invoke();
+            }
+
+            public async Task Connect(Uri peerUri)
+            {
+                await dataConnection.Connect(peerUri);
+            }
+
+            public async Task Disconnect()
+            {
+                await dataConnection.Disconnect();
+            }
+
+            public bool IsConnected()
+            {
+                return dataConnection.IsConnected();
+            }
+        }
+    }
+}
